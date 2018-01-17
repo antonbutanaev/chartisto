aefcdac7b6c7e03fc46c1d5cafa5829379d45321
Anton Butanaev <abutanaev@ac-sw.com>
2018-01-17 11:02:01 +0700

    Добавлен gtest

---
+++ test/testpainter.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+#include <painter.h>
+
+TEST(Test1, Test) {
+	Painter painter;
+	EXPECT_EQ(1,1);
+}
+
+TEST(Test1, Test2) {
+	Painter painter;
+	EXPECT_EQ(1,1);
+}
+
+TEST(Test1, Test3) {
+	Painter painter;
+	EXPECT_EQ(1,1);
+}
+
+TEST(Test2, A) {
+	Painter painter;
+	EXPECT_EQ(1,1);
+}
+
+TEST(Test2, B) {
+	Painter painter;
+	EXPECT_EQ(1,1);
+}
+
+
