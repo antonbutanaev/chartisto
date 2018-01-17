9d4e55780e8b8d1cbedda6c9daeda223ebd786a0
Антон <a71b@ya.ru>
2018-01-17 23:02:37 +0700

    Создание интерфейсов

--- chart.h
+++ chart.h
@@ -1,7 +1,32 @@
-#ifndef PAINTER_H
-#define PAINTER_H
+#ifndef CHART_H
+#define CHART_H
+
+#include <cstddef>
 
 namespace chart {
+
+using Volume = double;
+using Price = double;
+const Price NoPrice = 0;
+
+struct Bar {
+    virtual ~Bar() = default;
+
+    virtual Price open() const = 0;
+    virtual Price close() const = 0;
+    virtual Price high() const = 0;
+    virtual Price low() const = 0;
+
+    virtual Volume volume() const = 0;
+};
+
+class Chart {
+public:
+    virtual ~Chart() = default;
+
+    virtual size_t numBars() const = 0;
+    virtual const Bar &bar(size_t i) const = 0;
+};
 
 class Canvas {
 public:
