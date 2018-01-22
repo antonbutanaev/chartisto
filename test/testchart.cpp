6c954d7ed8ecd5ca610e93ba1c2eb53ee4485a45
Anton Butanaev <abutanaev@ac-sw.com>
2018-01-22 09:44:32 +0700

    Добавлен тест на изменение размера полотна

--- test/testchart.cpp
+++ test/testchart.cpp
@@ -53,3 +53,23 @@ TEST(TestChart, AddCharts) {
 	EXPECT_EQ(canvas.chart(1).h(), 25*3/4);
 	EXPECT_EQ(canvas.chart(2).h(), 25);
 }
+
+TEST(TestChart, ResizeCharts) {
+	Canvas canvas;
+	canvas.setCanvasSize({100, 100});
+	EXPECT_EQ(canvas.numCharts(), 0);
+	canvas.addChart(Chart());
+	EXPECT_EQ(canvas.numCharts(), 1);
+	EXPECT_EQ(canvas.chart(0).h(), 100);
+
+	canvas.addChart(Chart());
+	EXPECT_EQ(canvas.numCharts(), 2);
+	EXPECT_EQ(canvas.chart(0).h(), 75);
+	EXPECT_EQ(canvas.chart(1).h(), 25);
+
+	canvas.setCanvasSize({200, 200});
+	EXPECT_EQ(canvas.numCharts(), 2);
+	EXPECT_EQ(canvas.chart(0).h(), 2*75);
+	EXPECT_EQ(canvas.chart(1).h(), 2*25);
+
+}
