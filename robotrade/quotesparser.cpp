c7b8ff808d4cdaeacc14d15b14389dd60a5e1d91
Anton Butanaev <abutanaev@ac-sw.com>
2018-02-07 11:41:39 +0700

    Подчистка

--- robotrade/quotesparser.cpp
+++ robotrade/quotesparser.cpp
@@ -26,26 +26,20 @@ void parse(std::istream &is) {
     // VTBR,D,20130108,000000,0.0550000,0.0554000,0.0548300,0.0549200,23315530000
     const regex re(R"RE(^(\w+),D,(\d{4})(\d{2})(\d{2}),(\d{2})(\d{2})\d{2},([\d.]+),([\d.]+),([\d.]+),([\d.]+),(\d+))RE");
 
-    int i=0;
     while (is) {
         getline(is, line);
         line.pop_back();
-
-        if(++i > 10)
-            break;
 
         cout << line << endl;
         smatch match;
         if (regex_search(line, match, re)) {
 
             const auto date =
-                  sys_days{year{stoi(match[2])} / stoi(match[3]) / stoi(match[4])} +
-    hours{stoi(match[5])} + minutes{stoi(match[6])};
+                 sys_days{year{stoi(match[2])} / stoi(match[3]) / stoi(match[4])} +
+                 hours{stoi(match[5])} + minutes{stoi(match[6])};
 
-
-    chart::Time t = date;
-
-            cout << "MA:" << t << endl;
+			chart::Time t = date;
+			cout << "MA:" << t << endl;
         }
     }
 }
