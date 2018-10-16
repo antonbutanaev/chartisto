b89b9f2ea2e565cc6a611a559416fd51bfcf9644
Антон <antonbutanaev@gmail.com>
2018-10-16 21:21:39 +0700

    Подстройка общих параметров

--- robotrade/Levels.h
+++ robotrade/Levels.h
@@ -20,8 +20,8 @@ struct FindLevelsParams {
 	double maxCrossRate = 0.4;
 	size_t minExtremumAgeBars = 20;
 	size_t minTouches = 3;
-	size_t numBarsForLevel = 400;
-	double levelBodyCrossPrecisionK = 0.005;
+	size_t numBarsForLevel = 250;
+	double levelBodyCrossPrecisionK = 0.002;
 	size_t numBarsComing = 5;
 	double crossLimitOrderAboveLimitK = 0.005;
 };
