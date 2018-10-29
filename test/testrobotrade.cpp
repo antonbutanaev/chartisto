7883af0ca75aceec48af25b91571207d3d4df5c5
Anton Butanaev <abutanaev@ac-sw.com>
2018-10-29 16:52:09 +0700

    Плюс 3 теста на продажу

--- test/testrobotrade.cpp
+++ test/testrobotrade.cpp
@@ -479,6 +479,25 @@ TEST(TestRobotrade, EntryAnalyzer) {
 	{
 		string quotes =
 			"<TICKER>,<PER>,<DATE>,<TIME>,<OPEN>,<HIGH>,<LOW>,<CLOSE>,<VOL>\n"
+			"X,D,20180207,000000,10,10,10,10,10\n"
+			"X,D,20180208,000000,10,20,10,13.01,10\n";
+		stringstream ss(quotes);
+
+		const auto bars = parse(ss);
+		ProbabilityProvider probabilityProvider(1);
+		EntryAnalyzer entryAnalyzer({}, bars, probabilityProvider, cout);
+		const auto result = entryAnalyzer.analyze(
+			EntryAnalyzer::Direction::Sell, 9,11,1, 0,1
+		);
+		EXPECT_FALSE(result.stopped);
+		EXPECT_FALSE(result.profit);
+		EXPECT_FALSE(result.filled);
+		ASSERT_TRUE(result.runAway);
+		EXPECT_NEAR(result.runAway->price, 13, PriceEpsilon);
+	}
+	{
+		string quotes =
+			"<TICKER>,<PER>,<DATE>,<TIME>,<OPEN>,<HIGH>,<LOW>,<CLOSE>,<VOL>\n"
 			"X,D,20180207,000000,1,1,1,1,1\n"
 			"X,D,20180208,000000,10,25,5,20,1000\n";
 		stringstream ss(quotes);
@@ -502,6 +521,29 @@ TEST(TestRobotrade, EntryAnalyzer) {
 	{
 		string quotes =
 			"<TICKER>,<PER>,<DATE>,<TIME>,<OPEN>,<HIGH>,<LOW>,<CLOSE>,<VOL>\n"
+			"X,D,20180207,000000,10,10,10,10,10\n"
+			"X,D,20180208,000000,10,11.01,4,10,10\n";
+		stringstream ss(quotes);
+
+		const auto bars = parse(ss);
+		ProbabilityProvider probabilityProvider(1);
+		EntryAnalyzer entryAnalyzer({}, bars, probabilityProvider, cout);
+		const auto result = entryAnalyzer.analyze(
+			EntryAnalyzer::Direction::Sell, 9,11,5, 0,1
+		);
+		ASSERT_TRUE(result.stopped);
+		EXPECT_TRUE(result.stopped->probable);
+		EXPECT_FALSE(result.stopped->lossless);
+		EXPECT_EQ(result.stopped->time, sys_days{2018_y/feb/8});
+		EXPECT_FALSE(result.profit);
+		ASSERT_EQ(result.probablyNoProfit.size(), 1u);
+		EXPECT_EQ(result.probablyNoProfit[0], sys_days{2018_y/feb/8});
+		EXPECT_TRUE(result.filled);
+		EXPECT_FALSE(result.runAway);
+	}
+	{
+		string quotes =
+			"<TICKER>,<PER>,<DATE>,<TIME>,<OPEN>,<HIGH>,<LOW>,<CLOSE>,<VOL>\n"
 			"X,D,20180207,000000,1,1,1,1,1\n"
 			"X,D,20180208,000000,10,25,5,20,1000\n";
 		stringstream ss(quotes);
@@ -511,6 +553,28 @@ TEST(TestRobotrade, EntryAnalyzer) {
 		EntryAnalyzer entryAnalyzer({}, bars, probabilityProvider, cout);
 		const auto result = entryAnalyzer.analyze(
 			EntryAnalyzer::Direction::Buy, 11,6,24, 0,1
+		);
+		EXPECT_FALSE(result.stopped);
+		ASSERT_EQ(result.probablyNotStopped.size(), 1u);
+		EXPECT_EQ(result.probablyNotStopped[0], sys_days{2018_y/feb/8});
+		ASSERT_TRUE(result.profit);
+		EXPECT_TRUE(result.profit->probable);
+		EXPECT_EQ(result.profit->time, sys_days{2018_y/feb/8});
+		EXPECT_TRUE(result.filled);
+		EXPECT_FALSE(result.runAway);
+	}
+	{
+		string quotes =
+			"<TICKER>,<PER>,<DATE>,<TIME>,<OPEN>,<HIGH>,<LOW>,<CLOSE>,<VOL>\n"
+			"X,D,20180207,000000,10,10,10,10,10\n"
+			"X,D,20180208,000000,10,11.01,4.99,10,10\n";
+		stringstream ss(quotes);
+
+		const auto bars = parse(ss);
+		ProbabilityProvider probabilityProvider(2);
+		EntryAnalyzer entryAnalyzer({}, bars, probabilityProvider, cout);
+		const auto result = entryAnalyzer.analyze(
+			EntryAnalyzer::Direction::Sell, 9,11,5, 0,1
 		);
 		EXPECT_FALSE(result.stopped);
 		ASSERT_EQ(result.probablyNotStopped.size(), 1u);
