31a8cad5858f73cbc72a854dd8fb501fae951496
Anton Butanaev <abutanaev@ac-sw.com>
2018-11-01 12:37:44 +0700

    Перенос обновления котировок из robotrade в quotetool

--- robotrade/QuoteTool.cpp
+++ robotrade/QuoteTool.cpp
@@ -12,6 +12,7 @@
 
 using namespace std;
 using namespace date;
+using namespace chart;
 
 void splitQuotes(const string &quotesFile, string suffix) {
 	if (!suffix.empty())
@@ -54,11 +55,76 @@ void splitQuotes(const string &quotesFile, string suffix) {
 	}
 }
 
+void updateQuotes(const string &quoteUpdatesFile, const vector<string> &quoteFiles) {
+	struct Quotes {
+		string quoteFile;
+		data::PBars bars;
+	};
+
+	map<string, Quotes> quotesByTitle;
+	for (const auto &quoteFile: quoteFiles) {
+		Quotes quotes;
+		quotes.quoteFile = quoteFile;
+		ifstream ifs(quoteFile.c_str());
+		if (!ifs)
+			throw runtime_error("could not open " + quoteFile);
+		quotes.bars = robotrade::parse(ifs);
+		quotesByTitle[quotes.bars->title(0)] = move(quotes);
+	}
+
+	ifstream ifs(quoteUpdatesFile.c_str());
+	if (!ifs)
+		throw runtime_error("could not open " + quoteUpdatesFile);
+	const auto updateBars = robotrade::parse(ifs);
+
+	size_t added = 0;
+	for (size_t barNum = 0; barNum < updateBars->num(); ++barNum) {
+		const auto quotesIt = quotesByTitle.find(updateBars->title(barNum));
+		if (quotesIt == quotesByTitle.end()) {
+			cerr << "Update quotes, title " << updateBars->title(barNum) << " not found" << endl;
+			continue;
+		}
+
+		const auto &quotes = quotesIt->second;
+		ofstream ofs(quotes.quoteFile.c_str(), ios::app);
+		if (!ofs)
+			throw runtime_error("could not open " + quoteUpdatesFile);
+
+		ofs.precision(10);
+		auto lastBarNum = quotes.bars->num() - 1;
+		if (updateBars->time(barNum) > quotes.bars->time(lastBarNum)) {
+			++added;
+			const auto time = updateBars->time(barNum);
+			const auto day = chrono::time_point_cast<date::days>(time);
+			const auto ymd = year_month_day(day);
+			const auto tod = date::make_time(time - day);
+			ofs
+				<< updateBars->title(barNum) << ","
+				<< "D,"
+				<< setfill('0')
+				<< setw(4) << static_cast<int>(ymd.year())
+				<< setw(2) << static_cast<unsigned>(ymd.month())
+				<< setw(2) << static_cast<unsigned>(ymd.day()) << ","
+				<< setw(2) << tod.hours().count()
+				<< setw(2) << tod.minutes().count()
+				<< "00,"
+				<< updateBars->open(barNum) << ","
+				<< updateBars->high(barNum) << ","
+				<< updateBars->low(barNum) << ","
+				<< updateBars->close(barNum) << ","
+				<< fixed << setprecision(0) << updateBars->volume(barNum) << ",0"
+				<< endl;
+		}
+	}
+	cout << "Processed " << updateBars->num() << " update quotes, added " << added << endl;
+}
+
 int main(int ac, char *av[]) try {
 	const char
 		*argHelp = "help",
 		*argSuffix = "suffix",
 		*argQuotes = "quotes",
+		*argUpdateQuotes = "update-quotes",
 		*argSplitQuotes = "split-quotes",
 		*argLog = "log";
 
@@ -70,6 +136,7 @@ int main(int ac, char *av[]) try {
 	description.add_options()
 		(argHelp, "produce help message")
 		(argQuotes, po::value<vector<string>>(), "file with quotes")
+		(argUpdateQuotes, po::value<string>(), "file with quote updates")
 		(argSuffix, po::value<string>(), "add suffix to split files")
 		(argSplitQuotes, po::value<string>(), "split one multiticker quote file into separate files")
 		(argLog, po::value<string>()->default_value("robotrade_log.conf"), "log .conf file");
@@ -98,6 +165,11 @@ int main(int ac, char *av[]) try {
 
 	if (vm.count(argSplitQuotes)) {
 		splitQuotes(vm[argSplitQuotes].as<string>(), vm[argSuffix].as<string>());
+	} else if (vm.count(argUpdateQuotes)) {
+		const auto quotes = vm[argQuotes].as<vector<string>>();
+		if (quotes.empty())
+			throw runtime_error("No quotes to update");
+		updateQuotes(vm[argUpdateQuotes].as<string>(), quotes);
 	}
 
 	return 0;
--- robotrade/robotrade.cpp
+++ robotrade/robotrade.cpp
@@ -20,70 +20,6 @@ using namespace date;
 using namespace chart;
 using namespace std;
 
-void updateQuotes(const string &quoteUpdatesFile, const vector<string> &quoteFiles) {
-	struct Quotes {
-		string quoteFile;
-		data::PBars bars;
-	};
-
-	map<string, Quotes> quotesByTitle;
-	for (const auto &quoteFile: quoteFiles) {
-		Quotes quotes;
-		quotes.quoteFile = quoteFile;
-		ifstream ifs(quoteFile.c_str());
-		if (!ifs)
-			throw runtime_error("could not open " + quoteFile);
-		quotes.bars = robotrade::parse(ifs);
-		quotesByTitle[quotes.bars->title(0)] = move(quotes);
-	}
-
-	ifstream ifs(quoteUpdatesFile.c_str());
-	if (!ifs)
-		throw runtime_error("could not open " + quoteUpdatesFile);
-	const auto updateBars = robotrade::parse(ifs);
-
-	size_t added = 0;
-	for (size_t barNum = 0; barNum < updateBars->num(); ++barNum) {
-		const auto quotesIt = quotesByTitle.find(updateBars->title(barNum));
-		if (quotesIt == quotesByTitle.end()) {
-			cerr << "Update quotes, title " << updateBars->title(barNum) << " not found" << endl;
-			continue;
-		}
-
-		const auto &quotes = quotesIt->second;
-		ofstream ofs(quotes.quoteFile.c_str(), ios::app);
-		if (!ofs)
-			throw runtime_error("could not open " + quoteUpdatesFile);
-
-		ofs.precision(10);
-		auto lastBarNum = quotes.bars->num() - 1;
-		if (updateBars->time(barNum) > quotes.bars->time(lastBarNum)) {
-			++added;
-			const auto time = updateBars->time(barNum);
-			const auto day = chrono::time_point_cast<date::days>(time);
-			const auto ymd = year_month_day(day);
-			const auto tod = date::make_time(time - day);
-			ofs
-				<< updateBars->title(barNum) << ","
-				<< "D,"
-				<< setfill('0')
-				<< setw(4) << static_cast<int>(ymd.year())
-				<< setw(2) << static_cast<unsigned>(ymd.month())
-				<< setw(2) << static_cast<unsigned>(ymd.day()) << ","
-				<< setw(2) << tod.hours().count()
-				<< setw(2) << tod.minutes().count()
-				<< "00,"
-				<< updateBars->open(barNum) << ","
-				<< updateBars->high(barNum) << ","
-				<< updateBars->low(barNum) << ","
-				<< updateBars->close(barNum) << ","
-				<< fixed << setprecision(0) << updateBars->volume(barNum) << ",0"
-				<< endl;
-		}
-	}
-	cout << "Processed " << updateBars->num() << " update quotes, added " << added << endl;
-}
-
 void processLevels(
 	const string &configJson, int daysToAnalyze, const vector<string> &quoteFiles,
 	bool printSummary, bool printResultFiles, unsigned seed
@@ -177,7 +113,6 @@ int main(int ac, char *av[]) try {
 	const char
 		*argHelp = "help",
 		*argQuotes = "quotes",
-		*argUpdateQuotes = "update-quotes",
 		*argLevelsJson = "levels",
 		*argLevelsDays = "levels-days",
 		*argLevelsSummary = "levels-summary",
@@ -193,7 +128,6 @@ int main(int ac, char *av[]) try {
 	description.add_options()
 		(argHelp, "produce help message")
 		(argQuotes, po::value<vector<string>>(), "file with quotes")
-		(argUpdateQuotes, po::value<string>(), "file with quote updates")
 		(argLevelsJson, po::value<string>()->default_value("levels.json"), "levels .json file")
 		(argLevelsDays, po::value<int>()->default_value(0), "Days to analyze, 0 means all")
 		(argSeed, po::value<unsigned>()->default_value(0), "Seed for random generator")
@@ -223,9 +157,7 @@ int main(int ac, char *av[]) try {
 	}
 
 	if (vm.count(argQuotes)) {
-		if (vm.count(argUpdateQuotes)) {
-			updateQuotes(vm[argUpdateQuotes].as<string>(), vm[argQuotes].as<vector<string>>());
-		} else if (vm.count(argLevelsJson)) {
+		if (vm.count(argLevelsJson)) {
 			processLevels(
 				vm[argLevelsJson].as<string>(),
 				vm[argLevelsDays].as<int>(),
