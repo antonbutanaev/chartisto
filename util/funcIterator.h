e0351ad94bc0041d5c5f43958412b081d8c9fcea
Антон <antonbutanaev@gmail.com>
2018-11-05 14:44:23 +0700

    Улучшения funcIterator

--- util/funcIterator.h
+++ util/funcIterator.h
@@ -15,14 +15,27 @@ template <class Func, class OnItem> void iterateFunc(Func func, const OnItem &on
 
 template<class Value> auto funcRangeIterator(Value begin, Value end) {
 	return [i = begin, end] () mutable {
-		std::optional<decltype(i)> ret;
-		return i == end? ret : ret = i++;
+		std::optional<decltype(i)> result;
+		return i == end? result : result = i++;
 	};
 }
 
 template<class Container> auto funcIterator(Container &container) {
 	return [it = container.begin(), end = container.end()] () mutable {
 		return it == end? nullptr : &*it++;
+	};
+}
+
+template<class Container, class ItemPartAddr> auto funcIterator(Container &container, ItemPartAddr itemPartAddr) {
+	return [it = container.begin(), end = container.end(), itemPartAddr = std::move(itemPartAddr)] () mutable {
+		return it == end? nullptr : itemPartAddr(*it++);
+	};
+}
+
+template<class Container, class ItemTransform> auto funcIteratorTransform(Container &container, ItemTransform itemTransform) {
+	return [it = container.begin(), end = container.end(), itemTransform = std::move(itemTransform)] () mutable {
+		std::optional<decltype(itemTransform(*it))> result;
+		return it == end? result : result = itemTransform(*it++);
 	};
 }
 
