14ef3b986a2b18728ee3e03c8bae4261cbbf1731
Антон <antonbutanaev@gmail.com>
2021-01-09 00:19:33 +0300

    Улучшена итерация

--- screener/ProcessTickers.cpp
+++ screener/ProcessTickers.cpp
@@ -1,3 +1,4 @@
+#include <iterator>
 #include "ProcessTickers.h"
 
 using namespace std;
@@ -5,12 +6,9 @@ using namespace std;
 namespace tiingo {
 
 void processTickers(std::istream &tickers, const std::string &cacheDir) {
-	for (;;) {
-		string ticker;
-		tickers >> ticker;
-		if (!tickers)
-			break;
-		cout << ticker << endl;
+
+	for (auto tickerIt = istream_iterator<string>(tickers), end = istream_iterator<string>(); tickerIt != end; ++tickerIt) {
+		cout << *tickerIt << endl;
 	}
 
 	cout << cacheDir << endl;
