7eaf220efdf5d16b8369d20f583d3e84e5c4628d
Антон <antonbutanaev@gmail.com>
2021-02-12 23:14:20 +0300

    Подчистка

--- screener/ShowRet13612.cpp
+++ screener/ShowRet13612.cpp
@@ -15,6 +15,7 @@ void showRet13612(const Quotess &quotess) {
 		Rate ret13612;
 	};
 	vector<Line> lines;
+	lines.resize(quotess.size());
 
 	for(const auto &[ticker, quotes]: quotess)
 		lines.push_back({ticker, calcRet13612W(endDate, quotes)});
@@ -23,9 +24,8 @@ void showRet13612(const Quotess &quotess) {
 		return a.ret13612 > b.ret13612;
 	});
 
-	for(const auto &line: lines) {
+	for(const auto &line: lines)
 		cout << line.ticker << '\t' << line.ret13612 << endl;
-	}
 }
 
 void showRet13612(istream &tickers, const string &quotesDir) {
